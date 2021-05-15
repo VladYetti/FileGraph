d85279a1d5a7749858bb9a7c314c0edda1c7fa0d
Vladislav <vld.horobets@gmail.com>
2021-05-16 00:35:10 +0300

    Algorithms

--- bfsalgorithm.cpp
+++ bfsalgorithm.cpp
@@ -4,5 +4,22 @@ BFSAlgorithm::BFSAlgorithm(void) {}
 
 QString BFSAlgorithm::search(QString currDir, QString currFile)
 {
+    QQueue<QString>q;
+    q.push_back(currDir);
+
+    while(!q.empty())
+    {
+        QString tmp = q.front();
+        q.pop_front();
+
+        QDir dir(tmp);
+        dir.setFilter(QDir::Dirs);
+
+        for(auto&i: dir.entryInfoList())
+        {
+            if(i.isDir()) q.push_back(i.absolutePath());
+            else if(i.isFile() && i.fileName() == currFile) return i.absoluteFilePath();
+        }
+    }
     return "";
 }
--- dfsalgorithm.cpp
+++ dfsalgorithm.cpp
@@ -4,5 +4,22 @@ DFSAlgorithm::DFSAlgorithm(){}
 
 QString DFSAlgorithm::search(QString currDir, QString currFile)
 {
+    QStack<QString>st;
+    st.push(currDir);
+
+    while(!st.empty())
+    {
+        QString tmp = st.top();
+        st.pop();
+
+        QDir dir(tmp);
+        dir.setFilter(QDir::Dirs);
+
+        for(auto&i: dir.entryInfoList())
+        {
+            if(i.isDir()) st.push(i.absolutePath());
+            else if(i.isFile() && i.fileName() == currFile) return i.absoluteFilePath();
+        }
+    }
     return "";
 }
